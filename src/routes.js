a03cab8042a745026cde5bff1bca4dbd8d509db2
akhileshjambhale <103635234+akhileshjambhale@users.noreply.github.com>
2023-03-01 16:32:58 +0530

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,78 @@
+import Aboutus from "./components/layout/Aboutus";
+import MainContainer from "./components/layout/MainContainer";
+import Album from "./pages/Album";
+import ContactUs from "./pages/ContactUs ";
+import CompChamp from "./pages/courses/Compchamp";
+import DAC from "./pages/courses/Dac";
+import DBDA from "./pages/Dbda";
+import Faculty from "./pages/Faculty";
+import Gallary from "./pages/Gallary";
+import Home from "./pages/Home";
+import LittleChamp from "./pages/LittleChamp";
+import Mscit from "./pages/Mscit";
+import Placement from "./pages/Placement";
+import SeniorCitizen from "./pages/SeniorCitizen";
+
+export const routes = [
+  {
+    path: "/",
+    element: <MainContainer />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/placement",
+        element: <Placement />,
+      },
+      {
+        path: "/about",
+        element: <Aboutus />,
+      },
+      {
+        path: "/Faculty",
+        element:<Faculty/>
+      },
+      {
+        path:"/ContactUs",
+        element:<ContactUs/>
+      },
+      {
+        path:"/Gallary",
+        element:<Gallary/>
+      },
+      {
+        path:"/DAC",
+        element:<DAC/>
+      },
+      {
+        path:"/DBDA",
+        element:<DBDA/>
+      },
+      {
+        path:"/MSCIT",
+        element:<Mscit/>
+      },
+      {
+        path:"/LittleChamp",
+        element:<LittleChamp/>
+      },
+      {
+        path:"/COMPCHAMP",
+        element:<CompChamp/>
+      },
+      {
+        path:"/Seniorcitizen",
+        element:<SeniorCitizen/>
+      },
+      {
+        path:"/Album",
+        element:<Album/>
+      },
+     
+
+
+    ],
+  },
+];
